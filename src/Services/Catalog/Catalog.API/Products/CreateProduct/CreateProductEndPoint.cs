d07b644d7936d7c13acfbda4448cf38d8f100fea
datnv <vandatxyz@gmail.com>
2025-08-22 17:41:40 +0700

    bài số 53

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,32 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductRequest(string Name, decimal Price, string Description, List<string> Category, string ImageFile);
+
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                // Validate the request.
+                var command = request.Adapt<CreateProductCommand>();
+                // Send the command to the handler.
+                var result = await sender.Send(command);
+                // Map the result to the response.
+                var response = result.Adapt<CreateProductResponse>();
+                // Return the response with a 201 Created status code.
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            // Map the endpoint to the CreateProductCommand
+            .WithName("CreateProduct")
+            .WithSummary("Create Product")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Create a new product")
+            ;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    // This record represents the command to create a new product.
+    public record CreateProductCommand(string Name, decimal Price, string Description, List<string> Category, string ImageFile)
+        : ICommand<CreateProductResult>;
+
+    // This record represents the result of creating a product, containing the product ID.
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        // This is where you would typically inject your DbContext or repository to interact with the database.
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create the product.
+            var product = new Product
+            {
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Name = command.Name,
+                Price = command.Price,
+            };
+            // save it to the database.
+
+
+            // return the result with the product ID.
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}

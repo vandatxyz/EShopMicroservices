08ec4d71d0539f89b832deacc8f49f7df39d1cab
vandat <vandatxyz@gmail.com>
2025-09-09 21:13:29 +0700

    up

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,30 @@
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints;
+
+//- Accepts the order ID as a parameter.
+//- Constructs a DeleteOrderCommand.
+//- Sends the command using MediatR.
+//- Returns a success or not found response.
+
+//public record DeleteOrderRequest(Guid Id);
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(id));
+            var response = result.Adapt<DeleteOrderResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("DeleteOrder")
+        .WithSummary("Delete Order")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithDescription("Delete order by Id");
+    }
+}
---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints;
+
+//- Accepts pagination parameters.
+//- Constructs a GetOrdersQuery with these parameters.
+//- Retrieves the data and returns it in a paginated format.
+public record GetOrdersRequest(PaginationRequest PaginationRequest);
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+public class GetOrders : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] GetOrdersRequest request, ISender sender) =>
+        {
+            var query = request.Adapt<GetOrdersQuery>();
+            var result = await sender.Send(query);
+            var response = result.Adapt<GetOrdersResponse>();
+            return Results.Ok(response);
+        })
+            .WithName("GetOrders")
+            .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Orders")
+            .WithDescription("Get Orders with Pagination");
+    }
+}
